71d8da04bb367fae3f1ec263acb9fda18d8972bd
sebbyapudo <sebbyapudo@gmail.com>
2023-07-21 17:47:49 +0300

    Rendered the mark up dynamically

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 /* app.js */
+// SELECT ELEMENTS
+
+const productsEl = document.querySelector('.products');
+
+
+// RENDER PRODUCTS
+function renderProducts(){
+
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+            <div class="item">
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src=${product.imgSrc}>
+                    </div>
+                    <div class="desc">
+                        <h2>${product.name}1</h2>
+                        <h2><small>$</small>${product.price}</h2>
+                        <p>
+                          ${product.description}
+                        </p>
+                    </div>
+                    <div class="add-to-wishlist">
+                        <img src="./icons/heart.png" alt="add to wish list">
+                    </div>
+                    <div class="add-to-cart">
+                        <img src="./icons/bag-plus.png" alt="add to cart">
+                    </div>
+                </div>
+            </div>
+    `
+  })
+}
+
+renderProducts()
